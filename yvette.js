5039e3fe2221894d86739236813c56483b736e1e
yvette250 <yvetteumugwaneza936@gamil.com>
2025-03-12 12:21:17 +0200

    hi

---
+++ yvette.js
@@ -0,0 +1,26 @@
+//function culculate circumference of the circle
+var pi=3.14;
+var r=4;
+var cirmcuference;
+cirmcuference=2*pi*r;
+console.log(cirmcuference);
+
+
+//function culculate area of the circle
+let area=3.14*radius*radius
+return area;
+{
+    console.log(culculatearea(5));
+}
+
+//function culculate force
+let m=7;
+let a=8;
+let force=[mass,acceleration];
+let answer=force.reduce((a,b),(a*b));
+console.log(answer)
+
+
+
+
+
